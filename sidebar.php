4442c9979630adbe727d4f64b5829c47c4a09b6d
99839 <99839@163.com>
2023-04-04 21:45:47 +0800

    Update sidebar.php

--- sidebar.php
+++ sidebar.php
@@ -8,8 +8,6 @@
  */
 ?>
 
-<div class='c-sidebar'>
-  <?php if ( is_active_sidebar( 'post-sidebar' ) ) : ?>
-    <?php dynamic_sidebar( 'post-sidebar' ); ?>
-  <?php endif; ?>
-</div>
+<?php if ( is_active_sidebar( 'post-sidebar' ) ) : ?>
+  <?php dynamic_sidebar( 'post-sidebar' ); ?>
+<?php endif; ?>
