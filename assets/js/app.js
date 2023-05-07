bb90237aee5ca6f20fe4f482d67b83d13979400d
99839 <99839@163.com>
2023-05-07 20:09:41 +0800

    remove unuse javascript

--- assets/js/app.js
+++ assets/js/app.js
@@ -477,20 +477,4 @@
       });
   });
 
-  // target all anchor link elements
-  const links = document.querySelectorAll('.navigation__link');
-
-  links.forEach(link => {
-      link.addEventListener('click', e => {
-          e.preventDefault();
-          if (!link.classList.contains('active')) {
-              const active = document.querySelector('.navigation__link.active');
-              if (active !== null) {
-                  active.classList.remove('active');
-              }
-              link.classList.add('active');
-          }
-      });
-  });
-
 })(jQuery);
