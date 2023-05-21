13078680528447e1ef2b81decd3d666be3644602
99839 <99839@163.com>
2023-05-21 19:29:54 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -153,103 +153,6 @@ function iebase_setup() {
   add_filter( 'wp_list_categories', 'iebase_categories_postcount_filter' );
 
   /**
-  * Remove parentheses from archive list and add span class to posts count
-  */
-  function iebase_archive_postcount_filter( $variable ) {
-    $variable = str_replace( '(', '<span class="post-count"> ', $variable );
-    $variable = str_replace( ')', ' </span>', $variable );
-    return $variable;
-  }
-  add_filter( 'get_archives_link', 'iebase_archive_postcount_filter' );
-
-  /**
-  * Replace class on reply link
-  */
-  function iebase_replace_reply_link_class( $class ) {
-    $class = str_replace( "class='comment-reply-link", "class='ie-btn outline tiny comment-reply-link", $class );
-    return $class;
-  }
-  add_filter( 'comment_reply_link', 'iebase_replace_reply_link_class' );
-
-  /**
-  * Replace class on cancel reply link
-  */
-  function iebase_replace_cancel_reply_link_class( $cancel_comment_reply_link, $post = null ) {
-    $new = str_replace( '<a', '<a class="button outline tiny"', $cancel_comment_reply_link );
-    return $new;
-  }
-  add_filter( 'cancel_comment_reply_link', 'iebase_replace_cancel_reply_link_class', 10, 2 );
-
-  /*
-   * Change the comment reply link to use 'Reply to &lt;Author First Name>'
-   * https://raam.org/2013/personalizing-the-wordpress-comment-reply-link/
-   */
-  function iebase_add_comment_author_to_reply_link( $link, $args, $comment ) {
-
-    $comment = get_comment( $comment );
-
-    // If no comment author is blank, use 'Anonymous'
-    if ( empty( $comment->comment_author ) ) {
-      if ( !empty( $comment->user_id ) ) {
-        $user=get_userdata( $comment->user_id );
-        $author=$user->user_login;
-      } else {
-        $author = esc_html__('Anonymous', 'iebase');
-      }
-    } else {
-      $author = $comment->comment_author;
-    }
-
-    // If the user provided more than a first name, use only first name
-    if( strpos( $author, ' ' ) ) {
-      $author = substr( $author, 0, strpos( $author, ' ' ) );
-    }
-
-    // Replace Reply Link with "Reply to Author First Name>"
-    $reply_link_text = $args['reply_text'];
-    $link = str_replace( $reply_link_text, esc_html__( 'Reply to', 'iebase' ) . ' ' . $author, $link );
-
-    return $link;
-  }
-  add_filter( 'comment_reply_link', 'iebase_add_comment_author_to_reply_link', 10, 3 );
-
-  /**
-   * Add Placehoder in comment Form Field (Comment)
-   */
-  function iebase_comment_textarea_placeholder( $fields ) {
-    $fields['comment_field'] = str_replace(
-      '<textarea',
-      '<textarea placeholder="' . esc_html__( 'Comment', 'iebase' ) . '" ',
-      $fields['comment_field']
-    );
-
-    return $fields;
-  }
-  add_filter( 'comment_form_defaults', 'iebase_comment_textarea_placeholder' );
-
-  /**
-   * Custom Comment Form Fields
-   * Remove labels and add placeholders instead.
-   */
-  function iebase_custom_comment_form_fields( $fields ) {
-    $commenter = wp_get_current_commenter();
-    $req = get_option( 'require_name_email' );
-    $aria_req = ( $req ? " aria-required='true'" : '' );
-
-    $fields['author'] = '<div class="e-grid"><div class="e-grid__col e-grid__col--4-4-s e-grid__col--1-3-m">'.
-      '<p><input id="author" name="author" type="text" placeholder="' . esc_html__( 'Name', 'iebase' ) . ( $req ? ' *' : '' ) . '" value="' . esc_attr( $commenter['comment_author'] ) . '" size="20"' . $aria_req . ' /></p></div>';
-
-    $fields['email'] = '<div class="e-grid__col e-grid__col--4-4-s e-grid__col--1-3-m">'.
-      '<p><input id="email" name="email" type="text" placeholder="' . esc_html__( 'Email', 'iebase' ) . ( $req ? ' *' : '' ) . '" value="' . esc_attr( $commenter['comment_author_email'] ) . '" size="20"' . $aria_req . ' /></p></div>';
-
-    $fields['url'] = '<div class="e-grid__col e-grid__col--4-4-s e-grid__col--1-3-m">'.
-      '<p><input id="url" name="url" type="text" placeholder="' . esc_html__( 'Website', 'iebase' ) . '" value="' . esc_attr( $commenter['comment_author_url'] ) . '" size="20"' . $aria_req . ' /></p></div></div>';
-
-    return $fields;
-  }
-  add_filter( 'comment_form_default_fields', 'iebase_custom_comment_form_fields' );
-
-  /**
    * Returns the title escaped, to be used in the share URLs
    */
   function iebase_get_escaped_title() {
@@ -551,3 +454,13 @@ require get_template_directory() . '/inc/class-woocommerce.php';
  * Integrate Lazyload
  */
 require get_template_directory() . '/inc/class-images.php';
+
+/**
+ * Relpace Gravatar
+ */
+require get_template_directory() . '/inc/class-avatar.php';
+
+/**
+ * Cunstom Comments
+ */
+require get_template_directory() . '/inc/class-comments.php';
